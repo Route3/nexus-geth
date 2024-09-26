4ae309c4ad0c15dab3e747b0de4e97d2ea2cd82b
Igor Crevar <crewce@gmail.com>
2024-09-26 14:01:41 +0200

    abi decode fixed

--- core/vm/nexusprecompiles/nexus_bls_precompile.go
+++ core/vm/nexusprecompiles/nexus_bls_precompile.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
+	"strings"
 
 	bn256 "github.com/Ethernal-Tech/bn256"
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -26,37 +27,38 @@ var (
 	errNexusBlsInvalidInput     = errors.New("invalid input")
 	errNexusBlsQuorumNotReached = errors.New("quorum not reached")
 
-	nexusBlsMultiABIType, _ = abi.NewType("tuple", "", []abi.ArgumentMarshaling{
-		{
-			Name: "hash",
-			Type: "bytes32",
-		},
-		{
-			Name: "signature",
-			Type: "bytes",
-		},
-		{
-			Name: "publicKeys",
-			Type: "uint256[4][]",
-		},
-		{
-			Name: "bitmap",
-			Type: "uint256",
-		},
-	})
-
 	true32Byte  = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
 	false32Byte = make([]byte, 32)
+
+	nexusBlsABI, _ = abi.JSON(strings.NewReader(`[{
+		"outputs":[
+        	{"name":"hash","type":"bytes32"},
+        	{"name":"signature","type":"bytes"},
+        	{
+				"name":"validatorsData",
+				"type":"tuple[]",
+				"components":[
+					{"name":"key","type":"uint256[4]"}
+				]
+			},
+        	{"name":"bitmap","type":"uint256"}
+		],
+    	"name":"nexus",
+		"type":"function"
+	}]`))
 )
 
-type nexusBlsPrecompileTuple struct {
-	Hash       [32]byte      `json:"hash" abi:"hash"`
-	Signature  []byte        `json:"signature" abi:"signature"`
-	PublicKeys [][4]*big.Int `json:"publicKeys" abi:"publicKeys"`
-	Bitmap     *big.Int      `json:"bitmap" abi:"bitmap"`
+type nexusValidatorData struct {
+	Key [4]*big.Int `abi:"key"`
 }
 
-// ecrecover implemented as a native contract.
+type nexusBlsPrecompileParams struct {
+	Hash           [32]byte             `abi:"hash"`
+	Signature      []byte               `abi:"signature"`
+	ValidatorsData []nexusValidatorData `abi:"validatorsData"`
+	Bitmap         *big.Int             `abi:"bitmap"`
+}
+
 type nexusBlsPrecompile struct {
 	domain []byte
 }
@@ -70,33 +72,29 @@ func (c *nexusBlsPrecompile) Run(input []byte) ([]byte, error) {
 		return nil, errNexusBlsInvalidInput
 	}
 
-	dt, err := abi.Arguments{{Type: nexusBlsMultiABIType}}.Unpack(input[1:])
-	if err != nil {
-		return nil, fmt.Errorf("%w: unpack error - %w", errNexusBlsInvalidInput, err)
-	}
+	var inputObj nexusBlsPrecompileParams
 
-	inputObj, ok := abi.ConvertType(dt[0], new(nexusBlsPrecompileTuple)).(*nexusBlsPrecompileTuple)
-	if !ok {
-		return nil, errNexusBlsInvalidInput
+	if err := nexusBlsABI.UnpackIntoInterface(&inputObj, "nexus", input[1:]); err != nil {
+		return nil, err
 	}
 
 	if len(inputObj.Signature) > maxSignatureSize {
 		return nil, fmt.Errorf("%w: invalid signature size - %d", errNexusBlsInvalidInput, len(inputObj.Signature))
 	}
 
-	if len(inputObj.PublicKeys) > maxPublicKeys {
-		return nil, fmt.Errorf("%w: too many public keys - %d", errNexusBlsInvalidInput, len(inputObj.PublicKeys))
+	if len(inputObj.ValidatorsData) > maxPublicKeys {
+		return nil, fmt.Errorf("%w: too many public keys - %d", errNexusBlsInvalidInput, len(inputObj.ValidatorsData))
 	}
 
-	publicKeys := make([]*bn256.PublicKey, 0, len(inputObj.PublicKeys))
+	publicKeys := make([]*bn256.PublicKey, 0, len(inputObj.ValidatorsData))
 	bitmap := NewBitmap(inputObj.Bitmap)
 
-	for i, pkSerialized := range inputObj.PublicKeys {
+	for i, pkSerialized := range inputObj.ValidatorsData {
 		if !bitmap.IsSet(uint64(i)) {
 			continue
 		}
 
-		pubKey, err := bn256.UnmarshalPublicKeyFromBigInt(pkSerialized)
+		pubKey, err := bn256.UnmarshalPublicKeyFromBigInt(pkSerialized.Key)
 		if err != nil {
 			return nil, fmt.Errorf("%w: public key - %w", errNexusBlsInvalidInput, err)
 		}
@@ -104,7 +102,7 @@ func (c *nexusBlsPrecompile) Run(input []byte) ([]byte, error) {
 		publicKeys = append(publicKeys, pubKey)
 	}
 
-	quorumCnt := (len(inputObj.PublicKeys)*2)/3 + 1
+	quorumCnt := (len(inputObj.ValidatorsData)*2)/3 + 1
 	// ensure that the number of serialized public keys meets the required quorum count
 	if len(publicKeys) < quorumCnt {
 		return nil, errNexusBlsQuorumNotReached
